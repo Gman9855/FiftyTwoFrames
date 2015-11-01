d556c7a529ebfb704e6d7e4f5c30a9e69eff6714
Gershy Lev <gbay9855@yahoo.com>
2015-11-01 10:20:47 -0800

    Change build to 1.05

--- Fifty-Two Frames/FTFYearPopoverTableViewController.m
+++ Fifty-Two Frames/FTFYearPopoverTableViewController.m
@@ -30,12 +30,6 @@ static NSString * const reuseIdentifier = @"reuseIdentifier";
     [super viewDidLoad];
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:reuseIdentifier];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
