2f9dac0fc451f0221ecc61e84465858476bbfbb4
Gershy Lev <gbay9855@yahoo.com>
2015-12-17 11:01:30 -0800

    Increase size of collection view cells

--- Fifty-Two Frames/FTFPhotoCollectionGridViewController.m
+++ Fifty-Two Frames/FTFPhotoCollectionGridViewController.m
@@ -155,7 +155,7 @@
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     
-    return CGSizeMake(([UIScreen mainScreen].bounds.size.width - 46) / 3, ([UIScreen mainScreen].bounds.size.width - 46) / 3);
+    return CGSizeMake(([UIScreen mainScreen].bounds.size.width - 28) / 3, ([UIScreen mainScreen].bounds.size.width - 28) / 3);
 }
 
 #pragma mark - Scroll View Delegate
