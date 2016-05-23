55c0c982abd9485e2ba548c69b0d14c939168a14
Gershy Lev <gbay9855@yahoo.com>
2016-05-23 14:54:43 -0700

    Add more attributes to FTFImage

--- Fifty-Two Frames/FTFImage.h
+++ Fifty-Two Frames/FTFImage.h
@@ -15,6 +15,13 @@ typedef enum {
     FTFImageSizeSmall
 } FTFImageSize;
 
+typedef enum {
+    FTFImageCritiqueTypeRegular,
+    FTFImageCritiqueTypeShredAway,
+    FTFImageCritiqueTypeExtraSensitive,
+    FTFImageCritiqueTypeNotInterested
+} FTFImageCritiqueType;
+
 @interface FTFImage : NSObject
 
 @property (nonatomic, strong) NSString *title;
@@ -23,12 +30,23 @@ typedef enum {
 @property (nonatomic, assign) NSInteger likesCount;
 @property (nonatomic, strong) NSArray *comments;
 @property (nonatomic, strong) NSString *photoID;
+@property (nonatomic, assign) double aperture;
+@property (nonatomic, strong) NSString *apertureString;
+@property (nonatomic, assign) double shutterSpeed;
+@property (nonatomic, strong) NSString *shutterSpeedString;
+@property (nonatomic, assign) NSInteger focalLength;
+@property (nonatomic, strong) NSString *focalLengthString;
+@property (nonatomic, assign) NSInteger ISO;
+@property (nonatomic, strong) NSString *isoString;
+@property (nonatomic, assign) FTFImageCritiqueType critiqueType;
 @property (nonatomic, strong, readonly) NSURL *smallPhotoURL;
 @property (nonatomic, strong, readonly) NSURL *largePhotoURL;
 @property (nonatomic, assign) CGSize smallPhotoSize;
 @property (nonatomic, assign) CGSize largePhotoSize;
 
 @property (nonatomic, assign) BOOL isLiked;
+@property (nonatomic, assign) BOOL qualifiesForExtraCreditChallenge;
+@property (nonatomic, assign) BOOL fromNewFramer;
 
 - (instancetype)initWithImageURLs:(NSArray *)imageURLs;
 - (void)requestImageWithSize:(FTFImageSize)size completionBlock:(void(^)(UIImage *image, NSError *error, BOOL isCached))block;
