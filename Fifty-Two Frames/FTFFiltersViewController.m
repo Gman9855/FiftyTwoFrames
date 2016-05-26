34ef25c7cd5dfdc0065be7d141b6a8f15903d99b
Gershy Lev <gbay9855@yahoo.com>
2016-05-25 23:44:49 -0700

    Add checkboxes for sort options

--- Fifty-Two Frames/FTFFiltersViewController.m
+++ Fifty-Two Frames/FTFFiltersViewController.m
@@ -15,9 +15,38 @@
 
 @interface FTFFiltersViewController () <UITextFieldDelegate>
 
+typedef enum {
+    FTFFilterSectionNameSearch,
+    FTFFilterSectionExposure,
+    FTFFilterSectionMoreFilters,
+    FTFFilterSectionSortBy
+} FTFFilterSection;
+
+typedef enum {
+    FTFExposureSectionExposureDropdown,
+    FTFExposureSectionApertureSwitch,
+    FTFExposureSectionApertureSlider,
+    FTFExposureSectionShutterSpeedSwitch,
+    FTFExposureSectionShutterSpeedSlider,
+    FTFExposureSectionISOSwitch,
+    FTFExposureSectionISOSlider,
+} FTFExposureSection;
+
+typedef enum {
+    FTFMoreFiltersSectionFocalLengthSwitch,
+    FTFMoreFiltersSectionFocalLengthSlider,
+    FTFMoreFiltersSectionNewFramersSwitch,
+    FTFMoreFiltersSectionExtraCreditChallengeSwitch
+} FTFMoreFiltersSection;
+
+typedef enum {
+    FTFSortBySectionDropdown,
+    FTFSortBySectionDefault,
+    FTFSortBySectionLikes,
+    FTFSortBySectionComments
+} FTFSortBySection;
+
 @property (weak, nonatomic) IBOutlet UITextField *searchTextField;
-@property (weak, nonatomic) IBOutlet UISwitch *sortByLikesSwitch;
-@property (weak, nonatomic) IBOutlet UISwitch *sortByCommentsSwitch;
 @property (weak, nonatomic) IBOutlet UISwitch *apertureSwitch;
 @property (weak, nonatomic) IBOutlet UISwitch *focalLengthSwitch;
 @property (weak, nonatomic) IBOutlet UISwitch *shutterSpeedSwitch;
@@ -32,13 +61,23 @@
 @property (weak, nonatomic) IBOutlet FTFFocalLengthRangeSlider *focalLengthRangeSlider;
 @property (weak, nonatomic) IBOutlet FTFShutterSpeedRangeSlider *shutterSpeedRangeSlider;
 @property (weak, nonatomic) IBOutlet FTFISORangeSlider *ISORangeSlider;
+@property (weak, nonatomic) IBOutlet UIImageView *sortByDefaultCheckbox;
+@property (weak, nonatomic) IBOutlet UIImageView *sortByLikesCheckbox;
+@property (weak, nonatomic) IBOutlet UIImageView *sortByCommentsCheckbox;
+@property (weak, nonatomic) IBOutlet UIImageView *sortByDefaultImageView;
+@property (weak, nonatomic) IBOutlet UILabel *sortByLabel;
+@property (nonatomic, assign) BOOL exposureDropdownIsSelected;
+@property (nonatomic, assign) BOOL sortByDropdownIsSelected;
+@property (nonatomic, assign) BOOL sortByDefaultIsSelected;
+@property (nonatomic, assign) BOOL sortByLikesIsSelected;
+@property (nonatomic, assign) BOOL sortByCommentsIsSelected;
 
 @end
 
 @implementation FTFFiltersViewController
 
 - (NSArray *)switches {
-    return @[self.sortByLikesSwitch, self.sortByCommentsSwitch, self.apertureSwitch, self.focalLengthSwitch, self.shutterSpeedSwitch, self.extraCreditChallengeSwitch, self.framerNewSwitch];
+    return @[self.apertureSwitch, self.focalLengthSwitch, self.shutterSpeedSwitch, self.ISOSwitch, self.extraCreditChallengeSwitch, self.framerNewSwitch];
 }
 
 - (void)viewDidLoad {
@@ -47,18 +86,6 @@
     UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapReceivedInView:)];
     tapGestureRecognizer.cancelsTouchesInView = NO;
     [self.view addGestureRecognizer:tapGestureRecognizer];
-}
-
-- (IBAction)sortByLikesSwitchToggled:(UISwitch *)sender {
-    if (sender.isOn) {
-        [self.sortByCommentsSwitch setOn:NO animated:true];
-    }
-}
-
-- (IBAction)sortByCommentsSwitchToggled:(UISwitch *)sender {
-    if (sender.isOn) {
-        [self.sortByLikesSwitch setOn:NO animated:true];
-    }
 }
 
 - (void)tapReceivedInView:(UITapGestureRecognizer *)sender {
@@ -115,11 +142,9 @@
         ISOUpperValue = [NSNumber numberWithInteger:self.ISORangeSlider.upperValueISO];
     }
     
-    if (self.sortByLikesSwitch.isOn) {
+    if (self.sortByLikesIsSelected) {
         sortOrder = [NSNumber numberWithInt:FTFSortOrderLikes];
-    }
-    
-    if (self.sortByCommentsSwitch.isOn) {
+    } else if (self.sortByCommentsIsSelected) {
         sortOrder = [NSNumber numberWithInt:FTFSortOrderComments];
     }
     
@@ -168,74 +193,137 @@
 }
 
 - (IBAction)toggleSubmenuCellVisibility:(UISwitch *)sender {
-    [self.tableView beginUpdates];
-    [self.tableView endUpdates];
+    [self updateSubmenuCellVisibility];
 }
 
 - (BOOL)shouldSaveFilters {
-    return self.framerNewSwitch.isOn || self.extraCreditChallengeSwitch.isOn || self.apertureSwitch.isOn || self.focalLengthSwitch.isOn || self.shutterSpeedSwitch.isOn || self.ISOSwitch.isOn || self.sortByLikesSwitch.isOn || self.sortByCommentsSwitch.isOn || ![self.searchTextField.text isEqualToString:@""];
+    return ![self.searchTextField.text isEqualToString:@""] || self.framerNewSwitch.isOn || self.extraCreditChallengeSwitch.isOn || self.apertureSwitch.isOn || self.focalLengthSwitch.isOn || self.shutterSpeedSwitch.isOn || self.ISOSwitch.isOn;
 }
 
 #pragma mark - UITableViewDelegate 
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [super tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:1]];
-    BOOL exposureCellIsSelected = cell.isSelected;
-    
-    if (indexPath.section == 1 && indexPath.row == 1) {
-        if (exposureCellIsSelected) {
-            return 44;
-        } else {
-            return 0;
-        }
-    } else if (indexPath.section == 1 && indexPath.row == 2) {
-        if (self.apertureSwitch.on && exposureCellIsSelected) {
-            return 80;
-        } else {
-            return 0;
-        }
-    } else if (indexPath.section == 1 && indexPath.row == 3) {
-        if (exposureCellIsSelected) {
-            return 44;
-        } else {
-            return 0;
-        }
-    } else if (indexPath.section == 1 && indexPath.row == 4) {
-        if (self.shutterSpeedSwitch.isOn && exposureCellIsSelected) {
-            return 80;
-        } else {
-            return 0;
-        }
-    } else if (indexPath.section == 1 && indexPath.row == 5) {
-        if (exposureCellIsSelected) {
-            return 44;
-        } else {
-            return 0;
-        }
-    } else if (indexPath.section == 1 && indexPath.row == 6) {
-        if (self.ISOSwitch.isOn && exposureCellIsSelected) {
-            return 80;
-        } else {
-            return 0;
-        }
-    } else if (indexPath.section == 2 && indexPath.row == 1) {
-        if (self.focalLengthSwitch.isOn) {
-            return 80;
-        } else {
-            return 0;
-        }
+    switch (indexPath.section) {
+        case FTFFilterSectionExposure:
+            switch (indexPath.row) {
+                case FTFExposureSectionApertureSwitch:
+                    return self.exposureDropdownIsSelected ? 44 : 0;
+                case FTFExposureSectionApertureSlider:
+                    return self.apertureSwitch.on && self.exposureDropdownIsSelected ? 80 : 0;
+                case FTFExposureSectionShutterSpeedSwitch:
+                    return self.exposureDropdownIsSelected ? 44 : 0;
+                case FTFExposureSectionShutterSpeedSlider:
+                    return self.shutterSpeedSwitch.on && self.exposureDropdownIsSelected ? 80 : 0;
+                case FTFExposureSectionISOSwitch:
+                    return self.exposureDropdownIsSelected ? 44 : 0;
+                case FTFExposureSectionISOSlider:
+                    return self.ISOSwitch.on && self.exposureDropdownIsSelected ? 80 : 0;
+                default:
+                    break;
+            }
+            break;
+        case FTFFilterSectionMoreFilters:
+            switch (indexPath.row) {
+                case FTFMoreFiltersSectionFocalLengthSlider:
+                    return self.focalLengthSwitch.isOn ? 80 : 0;
+                default:
+                    break;
+            }
+            break;
+        case FTFFilterSectionSortBy:
+            switch (indexPath.row) {
+                case FTFSortBySectionDropdown:
+                    break;
+                default:
+                    return !self.sortByDropdownIsSelected ? 0 : 44;
+            }
+        default:
+            break;
     }
     
     return 44;
 }
 
 - (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section == 1 && indexPath.row == 0) {
-        UITableViewCell *exposureCell = [self.tableView cellForRowAtIndexPath:indexPath];
-        [exposureCell setSelected:!exposureCell.isSelected];
-        [self.tableView beginUpdates];
-        [self.tableView endUpdates];
+    if (indexPath.section == FTFFilterSectionExposure && indexPath.row == FTFExposureSectionExposureDropdown) {
+        self.exposureDropdownIsSelected = !self.exposureDropdownIsSelected;
+        [self updateSubmenuCellVisibility];
+    } else if (indexPath.section == FTFFilterSectionSortBy) {
+        if (indexPath.row == FTFSortBySectionDropdown) {
+            self.sortByDropdownIsSelected = !self.sortByDropdownIsSelected;
+            [self updateSubmenuCellVisibility];
+            return nil;
+        }
+        
+        if (indexPath.row == FTFSortBySectionDefault) {
+            if (!self.sortByDefaultIsSelected) {
+                self.sortByDefaultIsSelected = YES;
+                self.sortByDropdownIsSelected = !self.sortByDropdownIsSelected;
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    self.sortByLabel.text = @"Default";
+                    [self updateSubmenuCellVisibility];
+                });
+                
+                if (self.sortByCommentsIsSelected) {
+                    self.sortByCommentsIsSelected = NO;
+                }
+                
+                if (self.sortByLikesIsSelected) {
+                    self.sortByLikesIsSelected = NO;;
+                }
+            }
+        }
+        
+        if (indexPath.row == FTFSortBySectionLikes) {
+            
+            if (!self.sortByLikesIsSelected) {
+                self.sortByLikesIsSelected = YES;
+                self.sortByDropdownIsSelected = !self.sortByDropdownIsSelected;
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    self.sortByLabel.text = @"Likes";
+                    [self updateSubmenuCellVisibility];
+                });
+                
+                if (self.sortByDefaultIsSelected) {
+                    self.sortByDefaultIsSelected = NO;
+                }
+                
+                if (self.sortByCommentsIsSelected) {
+                    self.sortByCommentsIsSelected = NO;
+                }
+            }
+        } else if (indexPath.row == FTFSortBySectionComments) {
+            if (!self.sortByCommentsIsSelected) {
+                self.sortByCommentsIsSelected = YES;
+                self.sortByDropdownIsSelected = !self.sortByDropdownIsSelected;
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    self.sortByLabel.text = @"Comments";
+                    [self updateSubmenuCellVisibility];
+                });
+                
+                if (self.sortByDefaultIsSelected) {
+                    self.sortByDefaultIsSelected = NO;
+                }
+                
+                if (self.sortByLikesIsSelected) {
+                    self.sortByLikesIsSelected = NO;;
+                }
+            }
+        }
+        
+        self.sortByCommentsCheckbox.image = [UIImage imageNamed:self.sortByCommentsIsSelected ? @"Checked" : @"Unchecked"];
+        self.sortByLikesCheckbox.image = [UIImage imageNamed:self.sortByLikesIsSelected ? @"Checked" : @"Unchecked"];
+        self.sortByDefaultCheckbox.image = [UIImage imageNamed:self.sortByDefaultIsSelected ? @"Checked" : @"Unchecked"];
+        CATransition *transition = [CATransition animation];
+        transition.duration = 0.3f;
+        transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        transition.type = kCATransitionFade;
+        
+        [self.sortByCommentsCheckbox.layer addAnimation:transition forKey:nil];
+        [self.sortByLikesCheckbox.layer addAnimation:transition forKey:nil];
+        [self.sortByDefaultCheckbox.layer addAnimation:transition forKey:nil];
     }
+    
     return nil;
 }
 
@@ -260,17 +348,41 @@
     for (UISwitch *sw in switches) {
         if ([sw isOn]) {
             [sw setOn:NO];
+            // Hide subcells
             if (sw == self.apertureSwitch || sw == self.focalLengthSwitch || sw == self.shutterSpeedSwitch || sw == self.ISOSwitch) {
-                [self.tableView beginUpdates];
-                [self.tableView endUpdates];
+                [self updateSubmenuCellVisibility];
             }
         }
+    }
+    if (self.sortByDropdownIsSelected) {
+        self.sortByDropdownIsSelected = NO;
+        [self updateSubmenuCellVisibility];
+    }
+    
+    if (self.exposureDropdownIsSelected) {
+        self.exposureDropdownIsSelected = NO;
+        [self updateSubmenuCellVisibility];
+    }
+    
+    if (!self.sortByDefaultIsSelected) {
+        self.sortByCommentsCheckbox.image = [UIImage imageNamed:@"Unchecked"];
+        self.sortByLikesCheckbox.image = [UIImage imageNamed:@"Unchecked"];
+        self.sortByDefaultCheckbox.image = [UIImage imageNamed:@"Checked"];
+        self.sortByCommentsIsSelected = NO;
+        self.sortByLikesIsSelected = NO;
+        self.sortByDefaultIsSelected = YES;
+        self.sortByLabel.text = @"Default";
     }
     
     [self.focalLengthRangeSlider resetKnobs];
     [self.apertureRangeSlider resetKnobs];
     
     self.searchTextField.text = @"";
+}
+
+- (void)updateSubmenuCellVisibility {
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
 }
 
 @end
