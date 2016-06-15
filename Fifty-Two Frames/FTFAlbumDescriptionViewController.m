030807e87fe375a7ef6eef412104a5f89400d991
Gershy Lev <gbay9855@yahoo.com>
2016-06-14 22:11:35 -0700

    Resize frame of albumInfoVC

--- Fifty-Two Frames/FTFAlbumDescriptionViewController.m
+++ Fifty-Two Frames/FTFAlbumDescriptionViewController.m
@@ -19,7 +19,6 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)setAlbum:(FTFAlbum *)album {
@@ -29,42 +28,17 @@
 
 - (void)_updateText;
 {
-//    self.title = self.album.name;
-//    [self.navigationController.navigationBar setTitleTextAttributes:
-//     @{NSForegroundColorAttributeName:[UIColor orangeColor]}];
     self.textView.text = self.album.info;
 }
 
 - (void)viewDidLoad;
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.75];
-    self.textView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.0];
     [self _updateText];
 }
 
 - (IBAction)tappedInView:(UITapGestureRecognizer *)sender {
     [self dismissViewControllerAnimated:true completion:nil];
 }
-
-- (void)viewWillAppear:(BOOL)animated {
-    [self.textView setContentOffset:CGPointZero animated:YES];
-//    CGRect newFrame = self.view.frame;
-//    
-//    newFrame.size.width = 200;
-//    newFrame.size.height = 200;
-//    [self.view setFrame:newFrame];
-}
-
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
